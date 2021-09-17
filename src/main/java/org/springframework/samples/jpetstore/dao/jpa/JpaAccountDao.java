7257dac7cde798d54dc578ed20c8618659fbc4cd
JayHung <youngjay9@gmail.com>
2021-09-18 07:11:17 +0800

    Ioc container: ApplicationContext.

---
+++ src/main/java/org/springframework/samples/jpetstore/dao/jpa/JpaAccountDao.java
@@ -0,0 +1,8 @@
+package org.springframework.samples.jpetstore.dao.jpa;
+
+public class JpaAccountDao {
+
+  public String getAccountInfo(String idNo) {
+    return "103338899";
+  }
+}
---
+++ src/main/java/org/springframework/samples/jpetstore/services/PetStoreService.java
@@ -0,0 +1,5 @@
+package org.springframework.samples.jpetstore.services;
+
+public interface PetStoreService {
+    String providedService();
+}
