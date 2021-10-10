c0c2c36462d9044e7237e8ea4b5a8fb48529747f
JayHung <youngjay9@gmail.com>
2021-10-10 14:30:25 +0800

    add ApplicationContextAware.

--- src/main/java/org/springframework/context/ApplicationContextAwareExample.java
+++ src/main/java/org/springframework/context/ApplicationContextAwareExample.java
@@ -11,15 +11,18 @@ public class ApplicationContextAwareExample {
 
     /*
       因為 singleton bean 只會在 spring 的 Ioc container 啟動時載入(只有一次),
-      當後續有 singleton bean 需要存取其它 prototype bean 時只能在 IoC container
-      第一次載入 singleton bean 時透過 setter 取得 prototype bean;
-      SpringFramework 提出了一個 API: ApplicationContextAware,只要實作它,
+      當 singleton bean 需要存取其它 prototype bean 時只能在 IoC container
+      第一次載入時透過 setter 取得 prototype bean;
+      SpringFramework 提出了一個 API: ApplicationContextAware;
       當有 bean 需要存取時, 它會自動執行 setApplicationContext 取得最新的 Ioc container.
     */
     IocContainerAware containerAware = context
         .getBean("iocContainerAware", IocContainerAware.class);
 
-    containerAware.getPrototypeBean(PrototypeBean.class);
+    PrototypeBean prototypeBean = (PrototypeBean) containerAware
+        .getPrototypeBean(PrototypeBean.class);
+
+    System.out.println(prototypeBean.getName());
 
   }
 }
--- src/main/java/org/springframework/dependency/examples/PrototypeBean.java
+++ src/main/java/org/springframework/dependency/examples/PrototypeBean.java
@@ -2,4 +2,11 @@ package org.springframework.dependency.examples;
 
 public class PrototypeBean {
 
+  public PrototypeBean() {
+    System.out.println("Prototype bean init!!");
+  }
+
+  public String getName() {
+    return "Prototype Bean";
+  }
 }
